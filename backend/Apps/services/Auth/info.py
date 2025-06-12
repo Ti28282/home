e3a4125daf665fc7457cb638d3503b89083a179a
Timur <sabirovt50@gmail.com>
2025-06-12 22:01:26 +0300

    Update backend

---
+++ backend/Apps/services/Auth/info.py
@@ -0,0 +1,20 @@
+### This File for Info ### 
+
+
+
+#! Auth service 
+
+# * Routes
+# todo /verification - POST ->  (JWT Access Token Cookies take Refresh Token) -< else >- (Error message type)
+# todo /authentication - POST -> (redirecting user to React server)  -< else >- (Error not redirecting user) 
+
+
+# ? Service helpers
+"""
+Docker Contianer
+
+Postgresql - connect to database and save user data
+
+
+"""
+
